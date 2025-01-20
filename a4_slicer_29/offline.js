dab0b64182618ca384cb107e3927417b928136d4
kodchakorn29 <s42887@bj.ac.th>
2025-01-20 16:02:57 +0700

    Add files via upload

---
+++ a4_slicer_29/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1737363501,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/strawberry-sheet0.png",
+		"images/topcut-sheet0.png",
+		"images/grape_w-sheet0.png",
+		"images/grape_top-sheet0.png",
+		"images/grape_bot-sheet0.png",
+		"images/spawn-sheet0.png",
+		"images/fruitslash-sheet0.png",
+		"images/botcut2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/topcut2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
